8c83793d392aef1bb127a64dd1555bae92a570c8
Adrian Czarnous <czarnous.adrian@gmail.com>
2021-06-21 15:19:02 +0200

    Add global styles to project
    add customs font-family, font-size, colors to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@ module.exports = {
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+
+      //add bg-<class-name> to class
+      colors: {
+        'main-color': "#4118EA",
+        'warning-color': '#FF6565',
+        'basic-font-color:': '#343434',
+        'seperator-color': "#e5e5e5",
+      },
+      fontSize: {
+        'base': '18px',
+        'large': '20px',
+        'xl': '48px'
+
+      }, fontFamily: {
+        'sans': ['Ubuntu', 'sans-serif']
+      }
+    },
   },
   variants: {
     extend: {},
