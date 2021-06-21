9f5a3a70bee2f52e058e140ed9cb72d72add103f
Mateusz Król <matek8952@gmail.com>
2021-06-21 16:02:35 +0200

    Common input without styles

--- src/components/common/Input.vue
+++ src/components/common/Input.vue
@@ -1,12 +1,15 @@
 <template>
-  <label>{{ label }}</label>
-  <input
-    type="text"
-    :id="id"
-    :placeholder="placeholder"
-    :value="modelValue"
-    @input="$emit('update:modelValue', $event.target.value)"
-  />
+  <div class="w-full flex flex-col items-start">
+    <label class="max-w-full">{{ label }}</label>
+    <input
+      class="max-w-full"
+      type="text"
+      :id="id"
+      :placeholder="placeholder"
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+    />
+  </div>
 </template>
 
 <script>
