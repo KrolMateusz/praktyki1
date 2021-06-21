beee3fff4a755c28a1184e71fdfbe7b193ccb589
Mateusz Król <matek8952@gmail.com>
2021-06-21 15:34:47 +0200

    v-model binding

---
+++ src/components/common/Input.vue
@@ -0,0 +1,22 @@
+<template>
+  <label>{{ label }}</label>
+  <input
+    type="text"
+    :id="id"
+    :placeholder="placeholder"
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+  />
+</template>
+
+<script>
+export default {
+  name: "FormText",
+  props: {
+    id: String,
+    label: String,
+    placeholder: String,
+    modelValue: String,
+  },
+};
+</script>
