3c8abd3cbb6d9367bc84ad131a80f64dbb2d5f33
Adrian Czarnous <czarnous.adrian@gmail.com>
2021-06-22 13:33:14 +0200

    create nav component

---
+++ src/components/Navigation/Navigation.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav class="px-40 py-8 bg-blue-300">
+    <router-link class="link" to="/">Home</router-link>
+    <router-link class="link" to="about">About</router-link>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.link {
+        @apply mr-12 text-white text-3xl relative
+
+}
+
+nav::after {
+    content: '';
+    @apply absolute w-10 h-10 bg-white rounded-full inset-y-0
+}
+
+/* .router-link-active.router-link-exact-active.link::after {
+    content: '';
+    @apply absolute w-10 h-10 bg-white rounded-full inset-y-0
+} */
+</style>
