f73efc20a83069965826cee8eea7e33aa41e0644
web-lizzard <czarnous.adrian@gmail.com>
2021-06-22 23:43:42 +0200

    add dot animation, add Link Component

---
+++ src/components/Navigation/Link.vue
@@ -0,0 +1,24 @@
+<template>
+  <li>
+    <router-link
+      @click="$emit(getOffSetLeft)"
+      class="text-white text-3xl relative mr-10"
+      :to="to"
+      >{{ name }}</router-link
+    >
+  </li>
+</template>
+
+<script>
+export default {
+  emits: {
+    getOffSetLeft: null,
+  },
+  props: {
+    to: { type: String, required: true },
+    name: { type: String, required: true },
+  },
+};
+</script>
+
+<style></style>
--- src/components/Navigation/Navigation.vue
+++ src/components/Navigation/Navigation.vue
@@ -1,24 +1,60 @@
 <template>
-  <nav class="px-40 py-8 bg-blue-300">
-    <router-link class="link" to="/">Home</router-link>
-    <router-link class="link" to="about">About</router-link>
+  <nav class="px-40 py-8 relative bg-main">
+    <ul class="flex">
+      <Link
+        v-for="route in routeValues"
+        :key="route.name"
+        :to="route.path"
+        :name="route.name"
+        @getOffSetLeft="handleDotAnimation"
+      />
+      <div
+        ref="dot"
+        :style="{ transform: `translate(${position}px, 15px)` }"
+        class="
+          absolute
+          w-10
+          h-10
+          bg-main
+          rounded-full
+          bottom-0
+          left-0
+          border-4 border-white
+          transform
+          translate-y-5
+          transition-transform
+        "
+      ></div>
+    </ul>
   </nav>
 </template>
 
 <script>
-export default {};
+import { ref } from "vue";
+import { routes } from "@/router/index";
+import Link from "./Link.vue";
+
+export default {
+  components: {
+    Link,
+  },
+  setup() {
+    const position = ref(0);
+    const dot = ref(null);
+    const routeValues = ref(routes);
+
+    function handleDotAnimation(e) {
+      position.value = e.target.offsetLeft + dot.value.offsetWidth / 2;
+    }
+
+    return {
+      handleDotAnimation,
+      position,
+      dot,
+      routeValues,
+    };
+  },
+};
 </script>
 
-<style>
-.link {
-        @apply mr-12 text-white text-3xl relative
-
-}
-
-
-
-.router-link-active.router-link-exact-active.link::after {
-    content: '';
-    @apply absolute w-10 h-10 bg-white rounded-full inset-y-0
-}
-</style>
+<style></style>
