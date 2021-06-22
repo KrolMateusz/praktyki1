a1ac56d242ec01048466749a3c2dc21696e0f94c
Mateusz Król <matek8952@gmail.com>
2021-06-22 12:14:55 +0200

    RadioGroup added

---
+++ src/components/common/RadioGroup.vue
@@ -0,0 +1,28 @@
+<template>
+  <p>{{ groupLabel }}</p>
+  <div class="flex justify-between items-center">
+    <div v-for="option in options" :key="option.key">
+      <input
+        class="mr-1"
+        type="radio"
+        :id="option.key"
+        :value="option.value"
+        v-model="selected"
+      />
+      <label :for="option.key">{{ option.label }}</label>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    groupLabel: String,
+    options: Array,
+  },
+  data() {
+    return {
+      selected: "",
+    };
+  },
+};
+</script>
