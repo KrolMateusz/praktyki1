7cc7e5b2a40e47d602c228fbfe908cf832dbe231
Mateusz Król <matek8952@gmail.com>
2021-06-22 14:21:07 +0200

    Added method for udpating input value, renamed component

--- src/components/common/TextInput.vue
+++ src/components/common/TextInput.vue
@@ -2,25 +2,30 @@
   <div class="flex flex-col items-start" :class="classes">
     <label class="max-w-full font-medium">{{ label }}</label>
     <input
-      class="border border-solid border-black rounded-sm max-w-full"
-      type="text"
+      @input="updateValue"
       :id="id"
       :placeholder="placeholder"
       :value="modelValue"
-      @input="$emit('update:modelValue', $event.target.value)"
+      class="border border-solid border-black rounded-sm max-w-full"
+      type="text"
     />
   </div>
 </template>
 
 <script>
 export default {
-  name: "FormText",
+  name: "TextInput",
   props: {
     id: String,
     label: String,
     placeholder: String,
     modelValue: String,
     classes: String,
+  },
+  methods: {
+    updateValue(event) {
+      this.$emit("update:modelValue", event.target.value);
+    },
   },
 };
 </script>
