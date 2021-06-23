97b8605485d220cb4f7a99afe0ecb99283de53a0
Krzysztof Wanat <krzysiekwanat@gmail.com>
2021-06-23 13:33:20 +0200

    feature/praktyki1-008-radio_buttons

---
+++ src/common/RadioButtons.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="flex items-baseline my-8">
+    <label :key="index" v-for="(icon, index) in icons">
+      <input class="opacity-0 absolute" name="size" type="radio" value="m" />
+      <component :is="icon"></component>
+    </label>
+  </div>
+</template>
+
+<script>
+import PizzaIcon from "./icons/pizza.vue";
+import BurgerIcon from "./icons/burger.vue";
+import KebabIcon from "./icons/kebab.vue";
+
+export default {
+  name: "RadioButtons",
+  components: {
+    PizzaIcon,
+    BurgerIcon,
+    KebabIcon,
+  },
+  setup() {
+    const icons = [PizzaIcon, BurgerIcon, KebabIcon];
+
+    return {
+      icons,
+    };
+  },
+};
+</script>
+
+<style scoped>
+[type="radio"]:checked + svg {
+  @apply bg-blue-300 fill-current text-white rounded-full;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <RadioButtons />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import HelloWorld from "@/components/HelloWorld.vue";
+import RadioButtons from "../common/RadioButtons";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
     HelloWorld,
+    RadioButtons,
   },
 };
 </script>
