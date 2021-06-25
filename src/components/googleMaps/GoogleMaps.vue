daa7a6cd411d0da7e742742dc7818eecb23c2784
Adrian Czarnous <czarnous.adrian@gmail.com>
2021-06-25 12:27:59 +0200

    add google-loader dependency, display google map

--- src/components/googleMaps/GoogleMaps.vue
+++ src/components/googleMaps/GoogleMaps.vue
@@ -1,13 +1,48 @@
 <template>
   <div>
-    <div>Google API goes here</div>
+    <div ref="googleMap" class="map">Google API goes here</div>
   </div>
 </template>
 
 <script>
+import { Loader } from "@googlemaps/js-api-loader";
+import { onMounted, ref } from "vue";
 export default {
   name: "GoogleMaps",
+  setup() {
+    const mapOptions = {
+      center: {
+        lat: 54,
+        lng: 12,
+      },
+      zoom: 4,
+    };
+
+    const googleMap = ref(null);
+    const loader = new Loader({
+      apiKey: "AIzaSyBk1HDrCjUgyVzhowW85akNFEVwygpNNDY",
+      libraries: ["places"],
+      version: "weekly",
+    });
+
+    onMounted(() => {
+      loader
+        .load()
+        .then((google) => {
+          new google.maps.Map(googleMap.value, mapOptions);
+        })
+        .catch((err) => console.log(err));
+    });
+
+    return { googleMap };
+  },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.map {
+  height: 500px;
+  width: 500px;
+  overflow: visible;
+}
+</style>
