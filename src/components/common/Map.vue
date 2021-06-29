5822ed4fa780a49c725df64ee079797c432a1eb5
web-lizzard <czarnous.adrian@gmail.com>
2021-06-29 22:19:36 +0200

    style map container into grid, display map

---
+++ src/components/common/Map.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <div ref="mapContainer" class="w-220 h-144 bg-main"></div>
+  </div>
+</template>
+
+<script>
+import { onMounted, ref } from "vue";
+
+export default {
+  name: "Map",
+  props: {
+    center: Object,
+  },
+  setup(props) {
+    const mapContainer = ref(null);
+    const H = window.H;
+
+    const initalizeHereMap = (platform) => {
+      const mapTypes = platform.createDefaultLayers();
+
+      const map = new H.Map(mapContainer.value, mapTypes.vector.normal.map, {
+        zoom: 10,
+        center: props.center,
+      });
+
+      addEventListener("resize", () => map.getViewPort().resize());
+
+      // add behavior control
+      new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
+
+      // add UI
+      H.ui.UI.createDefault(map, mapTypes);
+      // End rendering the initial map
+    };
+
+    onMounted(() => {
+      const platform = new H.service.Platform({
+        apiKey: process.env.VUE_APP_GOOGLE_MAP_API_KEY,
+      });
+      initalizeHereMap(platform);
+    });
+
+    return {
+      mapContainer,
+    };
+  },
+};
+</script>
+
+<style scoped></style>
