2b6466548f52206754e45a3ba1696dc6f79a6cea
Mateusz Król <matek8952@gmail.com>
2021-06-29 11:14:03 +0200

    Google maps request

--- src/components/googleMaps/GoogleMaps.vue
+++ src/components/googleMaps/GoogleMaps.vue
@@ -1,12 +1,46 @@
 <template>
-  <div>
-    <div>Google API goes here</div>
+  <div class="flex justify-center w-screen">
+    <div ref="googleMap" class="w-1/2 visible h-160"></div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+import { Loader } from "@googlemaps/js-api-loader";
+import { onMounted, ref } from "vue";
 export default {
   name: "GoogleMaps",
+  setup() {
+    const googleMap = ref(null);
+    const loader = new Loader({
+      apiKey: process.env.VUE_APP_GOOGLE_MAP_API_KEY,
+      libraries: ["drawing", "geometry", "places", "visualization"],
+      version: "weekly",
+      region: "PL",
+    });
+
+    onMounted(async () => {
+      try {
+        const mapOptions = {
+          center: { lat: 51.107883, lng: 17.038538 },
+          zoom: 14,
+          region: "PL",
+        };
+        const google = await loader.load();
+        const map = new google.maps.Map(googleMap.value, mapOptions);
+        console.dir(map);
+        axios
+          .get(
+            `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Museum&key=${process.env.VUE_APP_GOOGLE_MAP_API_KEY}`
+          )
+          .then((res) => console.dir(res));
+      } catch (e) {
+        console.log(e);
+      }
+    });
+
+    return { googleMap };
+  },
 };
 </script>
 
