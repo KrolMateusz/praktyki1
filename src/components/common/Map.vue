92f4b50e9503183d1f44b618c1bc0cede0d41b30
Mateusz Król <matek8952@gmail.com>
2021-06-30 10:04:35 +0200

    Discover nearby restaurants

--- src/components/common/Map.vue
+++ src/components/common/Map.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="flex self-center justify-center">
     <div ref="mapContainer" class="w-220 h-144 bg-main"></div>
+    <button @click="find">SZUKAJ</button>
   </div>
 </template>
 
@@ -15,19 +16,20 @@ export default {
   setup(props) {
     const mapContainer = ref(null);
     const H = window.H;
+    const map = ref(null);
 
     const initializeHereMap = (platform) => {
       const mapTypes = platform.createDefaultLayers();
 
-      const map = new H.Map(mapContainer.value, mapTypes.vector.normal.map, {
+      map.value = new H.Map(mapContainer.value, mapTypes.vector.normal.map, {
         zoom: 14,
         center: props.center,
       });
 
-      addEventListener("resize", () => map.getViewPort().resize());
+      addEventListener("resize", () => map.value.getViewPort().resize());
 
       // add behavior control
-      new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
+      new H.mapevents.Behavior(new H.mapevents.MapEvents(map.value));
 
       // add UI
       H.ui.UI.createDefault(map, mapTypes);
@@ -41,8 +43,30 @@ export default {
       initializeHereMap(platform);
     });
 
+    const find = () => {
+      const H = window.H;
+      const platform = new H.service.Platform({
+        apiKey: process.env.VUE_APP_GOOGLE_MAP_API_KEY,
+      });
+      const service = platform.getSearchService();
+      service.discover(
+        {
+          q: "pizza",
+          at: "51.107883,17.038538",
+        },
+        (result) => {
+          // Add a marker for each location found
+          result.items.forEach((item) => {
+            map.value.addObject(new H.map.Marker(item.position));
+          });
+        },
+        alert
+      );
+    };
+
     return {
       mapContainer,
+      find,
     };
   },
 };
