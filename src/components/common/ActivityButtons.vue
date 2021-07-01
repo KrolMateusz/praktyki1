5a86ff106152a37db9542f9b14b12a14aa36c50f
Tomasz Dudek <t.dudeek@gmail.com>
2021-07-01 22:00:21 +0200

    added folder data with objects, added icons into data objects, refactor components, property sequence check

--- src/components/common/ActivityButtons.vue
+++ src/components/common/ActivityButtons.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="flex flex-col flex-nowrap w-full">
     <RadioButtons :icons="icons" @update:modelValue="changeActivity" />
-    <span>{{ activityOption }}</span>
   </div>
 </template>
 <script>
@@ -9,6 +8,7 @@ import { computed } from "vue";
 import { useStore } from "vuex";
 import RadioButtons from "@/components/common/RadioButtons";
 import { SET_ACTIVITY_OPTION } from "@/store/mutation-types.js";
+import activityOptionData from "@/data/activityOption.json";
 import ShoeIcon from "@/components/common/icons/shoe.vue";
 import RollerbladesIcon from "@/components/common/icons/rollerblades.vue";
 import BicycleIcon from "@/components/common/icons/bicycle.vue";
@@ -22,35 +22,12 @@ export default {
     const store = useStore();
     const activityOption = computed(() => store.state.activityOption);
     const changeActivity = (value) => {
-      console.log(value);
       store.commit(SET_ACTIVITY_OPTION, value);
     };
-    const icons = {
-      shoe: {
-        id: 1,
-        icon: ShoeIcon,
-        value: {
-          name: "shoe",
-          kcal: 700,
-        },
-      },
-      rollerblades: {
-        id: 2,
-        icon: RollerbladesIcon,
-        value: {
-          name: "rollerblades",
-          kcal: 500,
-        },
-      },
-      bicycle: {
-        id: 3,
-        icon: BicycleIcon,
-        value: {
-          name: "bicycle",
-          kcal: 400,
-        },
-      },
-    };
+    activityOptionData.shoe.icon = ShoeIcon;
+    activityOptionData.rollerblades.icon = RollerbladesIcon;
+    activityOptionData.bicycle.icon = BicycleIcon;
+    const icons = { ...activityOptionData };
 
     return {
       changeActivity,
