348eb02981f66ad255784ab7892854a20466f203
Mateusz Król <matek8952@gmail.com>
2021-07-01 12:39:18 +0200

    Restaurants marked on map with svg

--- src/components/common/Map.vue
+++ src/components/common/Map.vue
@@ -19,6 +19,8 @@ export default {
     const mapContainer = ref(null);
     const H = window.H;
     const map = ref(null);
+    const distanceInKm = 3;
+    const radiusInKm = 1;
 
     const initializeHereMap = (platform) => {
       const mapTypes = platform.createDefaultLayers();
@@ -45,17 +47,28 @@ export default {
       initializeHereMap(platform);
     });
 
-    const calculateCords = (start, distance) => {
+    const calculateCords = (start) => {
       const cords = [];
-      for (const lat of [distance / 100, -distance / 100]) {
+      for (const distance of [
+        (distanceInKm + radiusInKm) / 100,
+        -(distanceInKm + radiusInKm) / 100,
+      ]) {
         cords.push(
           {
-            lat: start.lat - lat,
+            lat: start.lat - distance,
             lng: start.lng,
           },
           {
             lat: start.lat,
-            lng: start.lng - lat,
+            lng: start.lng - distance,
+          },
+          {
+            lat: start.lat - distance,
+            lng: start.lng - distance,
+          },
+          {
+            lat: start.lat + distance,
+            lng: start.lng - distance,
           }
         );
       }
@@ -64,42 +77,39 @@ export default {
 
     const find = () => {
       const pizzaIcon = new H.map.Icon(pizza, { size: { w: 20, h: 20 } });
-      const distance = 2;
       const originLat = 51.117883;
       const originLng = 17.038538;
       const cords = calculateCords(
         { lat: originLat, lng: originLng },
-        distance
+        distanceInKm
       );
       const createBrowseQueryUrls = ({ lat, lng }) =>
-        `https://browse.search.hereapi.com/v1/browse?at=${lat},${lng}&foodTypes=800-057&circle:${lat},${lng};r=${
-          distance * 1000
-        }&limit=5&apiKey=${process.env.VUE_APP_GOOGLE_MAP_API_KEY}`;
-      const createRouteQueryUrls = (restaurant) =>
-        `https://router.hereapi.com/v8/routes?transportMode=pedestrian&origin=${originLat},${originLng}&destination=${restaurant.position.lat},${restaurant.position.lng}&return=travelSummary&apiKey=${process.env.VUE_APP_GOOGLE_MAP_API_KEY}`;
+        `https://browse.search.hereapi.com/v1/browse?at=${lat},${lng}&foodTypes=800-057&in=circle:${lat},${lng};r=${
+          radiusInKm * 1000
+        }&limit=20&apiKey=${process.env.VUE_APP_GOOGLE_MAP_API_KEY}`;
       const browseUrls = cords.map(createBrowseQueryUrls);
-
       const fetchData = (url) => axios.get(url);
+      const restaurants = [];
       const browseActions = browseUrls.map(fetchData);
+
       Promise.all(browseActions).then((result) => {
-        const restaurants = result.map((item) => item.data.items).flat();
-        console.log(restaurants);
-        const routeUrls = restaurants.map(createRouteQueryUrls);
-        const routeActions = routeUrls.map(fetchData);
-        Promise.all(routeActions).then((response) => {
-          const filteredResults = response.map(
-            (item) => item.data.routes[0].sections[0]
-          );
-          console.dir(filteredResults);
-          const filteredRestaurants = filteredResults.filter(
-            (item) => item.travelSummary.length >= distance * 1000
-          );
-          console.dir(filteredRestaurants);
-          filteredRestaurants.forEach((item) => {
-            map.value.addObject(
-              new H.map.Marker(item.arrival.place.location, { icon: pizzaIcon })
-            );
+        const places = result.map((item) => item.data.items).flat();
+        for (const place of places) {
+          console.log(place);
+          restaurants.push({
+            name: place.title,
+            address: place.address,
+            openingHours: place.openingHours
+              ? place.openingHours[0].text
+              : null,
+            position: place.position,
           });
+        }
+        console.log("RESTAURACJE", restaurants);
+        restaurants.forEach((item) => {
+          map.value.addObject(
+            new H.map.Marker(item.position, { icon: pizzaIcon })
+          );
         });
       });
     };
