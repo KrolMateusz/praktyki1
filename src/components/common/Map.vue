31b0aaec1a1f9097e560d27dbc561243ce50d82a
Mateusz Król <matek8952@gmail.com>
2021-07-02 10:52:08 +0200

    Review changes

--- src/components/common/Map.vue
+++ src/components/common/Map.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex self-center justify-center">
-    <div ref="mapContainer" class="w-220 h-144 bg-main"></div>
+    <div ref="mapContainer" class="w-220 h-144 bg-main" />
   </div>
 </template>
 
@@ -10,6 +10,12 @@ import { onMounted, ref } from "vue";
 
 export default {
   name: "Map",
+  props: {
+    center: {
+      type: Object,
+      default: () => ({ lat: 51.107883, lng: 17.038538 }),
+    },
+  },
   setup(props) {
     const store = useStore();
     const mapContainer = ref(null);
@@ -23,9 +29,6 @@ export default {
     return {
       mapContainer,
     };
-  },
-  props: {
-    center: Object,
   },
 };
 </script>
