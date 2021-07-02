98df0ddfaeab1e9e048249ab9af2473619ec587c
Adrian Czarnous <czarnous.adrian@gmail.com>
2021-07-02 11:33:30 +0200

    update mapModule action setMap

--- src/components/common/Map.vue
+++ src/components/common/Map.vue
@@ -7,39 +7,17 @@
 <script>
 import { useStore } from "vuex";
 import { onMounted, ref } from "vue";
-import H from "@here/maps-api-for-javascript";
 
 export default {
   name: "Map",
   setup(props) {
     const store = useStore();
     const mapContainer = ref(null);
-
-    const initializeHereMap = (platform) => {
-      const mapTypes = platform.createDefaultLayers();
-
-      const Map = new H.Map(mapContainer.value, mapTypes.vector.normal.map, {
-        zoom: 14,
-        center: props.center,
+    onMounted(() => {
+      store.dispatch("setMap", {
+        initPosition: props.center,
+        mapContainer: mapContainer.value,
       });
-
-      addEventListener("resize", () => Map.getViewPort().resize());
-
-      // add behavior control
-      new H.mapevents.Behavior(new H.mapevents.MapEvents(Map));
-
-      // add UI
-      H.ui.UI.createDefault(Map, mapTypes);
-      // End rendering the initial map
-      // store.dispatch("setMap", map.value)
-      store.commit("setMap", Map);
-    };
-
-    onMounted(async () => {
-      const platform = new H.service.Platform({
-        apiKey: process.env.VUE_APP_API_KEY,
-      });
-      initializeHereMap(platform);
     });
 
     return {
